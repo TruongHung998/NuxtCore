dc0a16f5c7d3659e1bbd8e7987d6a6821373726f
Mac <mac@Macbook-Pro-16inch-1TB.local>
2025-02-23 21:21:34 +0700

    _: update analytics

--- plugins/element-plus.ts
+++ plugins/element-plus.ts
@@ -2,7 +2,8 @@
 import { defineNuxtPlugin } from "#app";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
-
+import { inject } from "@vercel/analytics";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(ElementPlus);
+  inject();
 });
